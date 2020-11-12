ff67d5613a55e863e62eecff3e8fb130d5b86be4
19jy0132 <19jy0132@PC16.jynet2.jec.ac.jp>
2020-11-12 11:27:33 +0900

    HelloWorldJavaをHelloWorldPHPに変更

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -3,7 +3,7 @@ package project1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorldJava");
+		System.out.println("HelloWorldPHP");
 		System.out.println("Java");
 		System.out.println("add HTML");
 	}
